472674369c6da6d0b4678f1f1684998140695646
Momot17 <herifanantenanarakotomalala@gmail.com>
2023-02-05 22:15:14 +0300

    BACKEND: creation controller GET /api/users - user current

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -2,6 +2,28 @@ const User = require('../models/userModel');
 
 // GET /api/users - user current
 exports.getUserCurrent = (req, res, next) => {
-    User
-}
+    // TODO: Implement
+    User.findById(req.auth._userId)
+        .then(user => {
+            // check if user exists
+            if (!user) return res.status(401).json({
+                    message: 'User not found'
+                });
+            res.status(200).json({
+                message: 'User found',
+                user: user
+            });
+        });
+};
+
+// GET /api/users/:id - user by id
+exports.getUserById = (req, res, next) => {
+    // TODO: Implement
+};
+
+// PUT /api/users - Modify user current
+exports.putUserCurrent = (req, res, next) => {
+    // TODO: Implement
+};
+
 
