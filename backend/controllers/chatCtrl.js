03900292d6cdbfe99c6a051611460a00dfc3ccd3
Momot17 <herifanantenanarakotomalala@gmail.com>
2023-02-07 00:29:36 +0300

    BACKEND: creation controller GET /api/chat getAllChatUserCurrent

--- backend/controllers/chatCtrl.js
+++ backend/controllers/chatCtrl.js
@@ -3,7 +3,12 @@ const Chat = require('../models/chatModel');
 // GET /api/chat - all chat for userCurrent
 exports.getAllChatUserCurrent = (req, res, next) => {
     // TODO: Implement
-    
+    Chat.find({ "usersIdList.userId": req.auth._userId, "usersIdList.userId": req.body.userId })
+        .then(chatFound => res.status(200).json({
+            message: "Chat found!",
+            chat: chatFound 
+        }))
+        .catch(error => res.status(500).json({ error }));
 }
 // GET /api/chat/user/:id - get chat with an user
 exports.getChatUserById = (req, res, next) => {
