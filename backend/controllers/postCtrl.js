ea3b043393625cf75b269507d7ffcd1cd8a75e66
Momot17 <herifanantenanarakotomalala@gmail.com>
2023-02-06 12:33:20 +0300

    BACKEND: creation TODO controller postCtrl

---
+++ backend/controllers/postCtrl.js
@@ -0,0 +1,26 @@
+const Post = require('../models/postModel');
+
+// POST /api/post - create post
+exports.createPost = (req, res, next) => {
+    // TODO: Implement
+}
+
+// GET /api/post/user - all post of userCurrent
+exports.getAllPostUserCurrent = (req, res, next) => {
+    // TODO: Implement
+}
+
+// GET /api/post/user/:id - all post of an user
+exports.getAllPostUserById = (req, res, next) => {
+    // TODO: Implement
+}
+
+// PUT /api/post - like a post of an user
+exports.likePost = (req, res, next) => {
+    // TODO: Implement
+}
+
+// PUT /api/post - comment a post of an user
+exports.commentPost = (req, res, next) => {
+    // TODO: Implement
+}
