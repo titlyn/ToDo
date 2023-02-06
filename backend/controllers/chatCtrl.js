8e4fd4d137ce30ba4a3fbf7ff9b048ee3ff3de06
Momot17 <herifanantenanarakotomalala@gmail.com>
2023-02-07 00:14:47 +0300

    BACKEND: creation TODO controller chatCtrl

---
+++ backend/controllers/chatCtrl.js
@@ -0,0 +1,14 @@
+const Chat = require('../models/chatModel');
+
+// GET /api/chat - all chat for userCurrent
+exports.getAllChatUserCurrent = (req, res, next) => {
+    // TODO: Implement
+}
+// GET /api/chat/user/:id - get chat with an user
+exports.getChatUserById = (req, res, next) => {
+    // TODO: Implement
+}
+// POST /api/chat/create - create chat with an user
+exports.creatChat = (req, res, next) => {
+    // TODO: Implement
+}
